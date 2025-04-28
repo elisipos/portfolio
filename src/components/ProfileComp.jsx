d8992d6d012ca46a11fc12d64137bbb35135f6da
Eli Sipos <elijsipos@gmail.com>
2025-04-28 10:53:25 -0500

    working on profile text alignment

--- src/components/ProfileComp.jsx
+++ src/components/ProfileComp.jsx
@@ -9,19 +9,19 @@ const ProfileComp = () => {
   return (
     <div className="flex justify-center my-10">
 
-      <div className={`${theme == 'light' ? 'bg-black/5': 'bg-white/10'} w-3/4 lg:w-1/2 flex p-2 rounded-2xl`}>
+      <div className={`${theme == 'light' ? 'bg-black/5': 'bg-white/10'} w-4/5 lg:w-1/2 flex p-5 rounded-2xl`}>
       
         <div>
-          <div className={`${theme == 'light' ? 'border-black' : 'border-white'} w-20 h-20 rounded-full overflow-hidden border-2`}>
-            <img src={profileImg} alt="Picture of Eli Sipos" width="80" height="80"/>
+          <div className={`${theme == 'light' ? 'border-black' : 'border-white'} lg:w-20 lg:h-20 w-50 h-50 rounded-full overflow-hidden border-2`}>
+            <img src={profileImg} alt="Picture of Eli Sipos"/>
           </div>
         </div>
         
         <div className={`${theme == 'light' ? 'text-black' : 'text-white'} max-w-sm mx-4 flex items-center mt-5`}>
           <div>
-            <div className="text-3xl">Hi, I'm Eli Sipos.</div>
+            <div className="text-5xl lg:text-3xl flex">Hi, I'm Eli Sipos.</div>
 
-            <div className="flex items-center -m-0.5">
+            <div className="flex items-center text-lg">
               <span className="absolute animate-ping mx-1 mr-1.5 mt-1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} class="bi bi-circle-fill" viewBox="0 0 16 16">
                   <circle cx="8" cy="8" r="8"/>
