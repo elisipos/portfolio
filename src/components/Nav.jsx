033f3f9a4736dbe0076c4ef854ce8667c7547f67
Eli Sipos <elijsipos@gmail.com>
2025-04-30 12:16:37 -0500

    added animations for mobile dropdown

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'motion/react'
 import { ThemeContext } from '../ThemeContext'
 
 const Nav = () => {
@@ -6,6 +7,7 @@ const Nav = () => {
   const {theme, setTheme} = useContext(ThemeContext);
   const [text, setText] = useState(theme + " theme");
   const [prev, setPrev] = useState(text);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleClick = () => {
     theme == 'light' ? setTheme('dark') : setTheme('light');
@@ -29,6 +31,7 @@ const Nav = () => {
 
   return (
     <>
+    {/* Desktop Nav */}
     <div className={`${theme == "light" ? 'text-gray-200 bg-lime-500' : 'text-gray-800 bg-lime-400'} hidden sm:block`}>
       <div className={'flex justify-end p-5 lg:w-10/12'}>
         <div className='text-lg md:text-xl px-2 md:px-5 hover:cursor-pointer enlarge' onClick={() => handleClick()} onMouseOver={() => handleMouseEnter()} onMouseOut={() => handleMouseOut()}>{text}</div>
@@ -38,15 +41,36 @@ const Nav = () => {
       </div>
     </div>
 
+    {/* Mobile Nav Header */}
     <div className={`${theme == 'light' ? 'text-gray-200 bg-lime-500' : 'text-gray-800 bg-lime-400'} block sm:hidden h-15`}>
       <div className="flex justify-end p-3.5">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={`${theme == 'light' ? 'white' : 'black'}`} class="bi bi-list" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-        </svg>
+        <button onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle Menu" className="hover:cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={`${theme == 'light' ? 'white' : 'black'}`} className="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+        </button>
       </div>
     </div>
 
-    <div className="block sm:hidden"></div>
+    {/* Dropdown Menu */}
+    <AnimatePresence>
+      {menuOpen && (
+        <motion.div
+          key="dropdown"
+          initial={ { height: 0, opacity: 0 } }
+          animate={ { height: 'auto', opacity: 1 } }
+          exit={ { height: 0, opacity: 0} }
+          transition={ { duration: 0.3, ease: "easeInOut" } }
+          className={`${theme == 'light' ? 'text-gray-200 bg-lime-500' : 'text-gray-800 bg-lime-400'} origin-top`}>
+          <div className="flex flex-col items-end px-5 pb-4 space-y-3">
+            <div className="hover:cursor-pointer text-xl" onClick={() => handleClick()}>{text}</div>
+            <div className="hover:cursor-pointer text-xl">blogs</div>
+            <div className="hover:cursor-pointer text-xl">projects</div>
+            <div className="hover:cursor-pointer text-xl">contact</div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
     </>
   )
 }
--- src/components/ProfileComp.jsx
+++ src/components/ProfileComp.jsx
@@ -12,23 +12,23 @@ const ProfileComp = () => {
       <div className={`${theme == 'light' ? 'bg-black/5': 'bg-white/10'} flex p-5 rounded-2xl`}>
       
         <div className="flex items-center">
-          <div className={`${theme == 'light' ? 'border-black' : 'border-white'} lg:w-20 lg:h-20 w-50 h-50 rounded-full overflow-hidden border-2`}>
+          <div className={`${theme == 'light' ? 'border-black' : 'border-gray-200'} lg:w-25 lg:h-25 w-30 h-30 rounded-full overflow-hidden border-2`}>
             <img src={profileImg} alt="Picture of Eli Sipos"/>
           </div>
         </div>
         
-        <div className={`${theme == 'light' ? 'text-black' : 'text-white'} flex items-center mt-5`}>
+        <div className={`${theme == 'light' ? 'text-black' : 'text-gray-200'} flex items-center mt-5`}>
           <div>
-            <div className="text-5xl mx-5 flex">Hi, I'm Eli Sipos.</div>
+            <div className="text-3xl mx-1 lg:mx-5 flex">Hi, I'm Eli Sipos.</div>
 
             <div className="flex items-center mx-5 text-lg">
               <span className="absolute animate-ping mx-1 mr-1.5 mt-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} class="bi bi-circle-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} className="bi bi-circle-fill" viewBox="0 0 16 16">
                   <circle cx="8" cy="8" r="8"/>
                 </svg>
               </span>
               <span className="relative mx-1 mr-1.5 mt-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} class="bi bi-circle-fill" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} className="bi bi-circle-fill" viewBox="0 0 16 16">
                   <circle cx="8" cy="8" r="8"/>
                 </svg>
               </span>
