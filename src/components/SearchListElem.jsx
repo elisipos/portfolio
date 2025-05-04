304eafd8cbc5496bc8d198726eb41b5da0079b43
Eli Sipos <elijsipos@gmail.com>
2025-05-03 19:36:47 -0500

    tech stack looking good on mobile screens

--- src/components/SearchListElem.jsx
+++ src/components/SearchListElem.jsx
@@ -5,17 +5,28 @@ import { ThemeContext } from '../ThemeContext'
 const SearchListElem = (prop) => {
 
   const {theme, setTheme} = useContext(ThemeContext);
+  const [open, setOpen] = useState();
 
   return (
     <>
       <motion.div
-      key="li"
-      whileHover={ { scale: 1.05 } }
-      transition={ { duration: 0.1, ease: "easeInOut"} }
-      className={`${theme == 'light' ? 'text-black' : 'text-gray-300'} grid grid-cols-2 lg:grid-cols-4 justify-items-center m-2 py-2 rounded-lg hover:cursor-default`}>
-      <li className="w-min col-span-1 content-center">{prop.svg}</li>
-      <li className="max-w-xs col-span-1 content-center">{prop.id}</li>
-      <li className="max-w-xs hidden md:block lg:col-span-2 content-center">{prop.detail}</li>
+        key="li"
+        whileHover={ { scale: 1.05 } }
+        transition={ { duration: 0.1, ease: "easeInOut"} }
+        onClick={() => setOpen(!open)}
+        className={`${theme == 'light' ? 'text-black' : 'text-gray-300'} grid grid-cols-2 md:grid-cols-4 justify-items-center m-2 py-2 rounded-lg md:hover:cursor-default hover:cursor-pointer`}>
+        <li className="w-min col-span-1 content-center">{prop.svg}</li>
+        <li className="max-w-xs col-span-1 content-center">{prop.id}</li>
+        <li className="max-w-xs hidden md:block md:col-span-2 content-center">{prop.detail}</li>
+        {open && (<motion.div
+          key="details"
+          initial={ { height: 0, opacity: 0 } }
+          animate={ { height: 'auto', opacity: 1} }
+          exit={ { height: 0, opacity: 0 } }
+          transition={ { duration: 0.2, ease: "easeInOut" } }
+          className="col-span-2 origin-top md:hidden block">
+          {prop.detail}
+        </motion.div>)}
       </motion.div>
     </>
   )
