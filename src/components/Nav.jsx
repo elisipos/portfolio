5a2b698fb097cddc8e36f7de7ace8d33b1fece3b
Eli Sipos <elijsipos@gmail.com>
2025-05-07 14:39:54 -0500

    added vite, tailwind, and angular to list of tech in the tech stack, theres a problem with angulars svg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,7 +35,7 @@ const Nav = () => {
     <div className={`${theme == "light" ? 'text-gray-200 bg-lime-500' : 'text-gray-800 bg-lime-400'} hidden sm:block`}>
       <div className={'flex justify-end p-5 lg:w-10/12'}>
         <div className='text-lg md:text-xl px-2 md:px-5 hover:cursor-pointer enlarge' onClick={() => handleClick()} onMouseOver={() => handleMouseEnter()} onMouseOut={() => handleMouseOut()}>{text}</div>
-        <div className='text-lg md:text-xl px-2 md:px-5 hover:cursor-pointer enlarge'>blogs</div>
+        <div className='text-lg md:text-xl px-2 md:px-5 hover:cursor-pointer enlarge'>education</div>
         <div className='text-lg md:text-xl px-2 md:px-5 hover:cursor-pointer enlarge'>projects</div>
         <div className='text-lg md:text-xl px-2 md:px-5 hover:cursor-pointer enlarge'>contact</div>
       </div>
@@ -46,7 +46,7 @@ const Nav = () => {
       <div className="flex justify-end p-3.5">
         <button onClick={() => setMenuOpen(!menuOpen)} aria-label="Toggle Menu" className="hover:cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={`${theme == 'light' ? 'white' : 'black'}`} className="bi bi-list" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
           </svg>
         </button>
       </div>
@@ -64,7 +64,7 @@ const Nav = () => {
           className={`${theme == 'light' ? 'text-gray-200 bg-lime-500' : 'text-gray-800 bg-lime-400'} origin-top`}>
           <div className="flex flex-col items-end px-5 pb-4 space-y-3">
             <div className="hover:cursor-pointer text-xl" onClick={() => handleClick()}>{text}</div>
-            <div className="hover:cursor-pointer text-xl">blogs</div>
+            <div className="hover:cursor-pointer text-xl">education</div>
             <div className="hover:cursor-pointer text-xl">projects</div>
             <div className="hover:cursor-pointer text-xl">contact</div>
           </div>
