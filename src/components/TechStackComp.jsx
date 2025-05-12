b077bb46682a4252b87f35a006e211646186f2fe
Eli Sipos <elijsipos@gmail.com>
2025-05-11 19:42:59 -0500

    small format fix

--- src/components/TechStackComp.jsx
+++ src/components/TechStackComp.jsx
@@ -23,7 +23,7 @@ const TechStackComp = () => {
           <SearchBarComp sendParentData={handleSearchInput}></SearchBarComp>
 
           <SearchListHeader></SearchListHeader>
-          <ul className={`${theme == 'light' ? 'custom-scrollbar-light' : 'custom-scrollbar-dark'} grid grid-cols-1 max-h-[55vh] overflow-y-scroll overflow-x-hidden custom-scrollbar-dark`}>
+          <ul className={`${theme == 'light' ? 'custom-scrollbar-light' : 'custom-scrollbar-dark'} grid grid-cols-1 max-h-[55vh] overflow-y-scroll overflow-x-hidden`}>
             <SearchList input={inputFromSearch}></SearchList>
           </ul>
 
