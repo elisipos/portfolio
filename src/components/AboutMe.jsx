ceef1d09b5f4fa576a21de1100125762e2cffa36
Eli Sipos <elijsipos@gmail.com>
2025-05-14 18:55:37 -0500

    working on small hover animations for the nav bar

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -19,7 +19,7 @@ const AboutMe = () => {
             {`\tI am a `}
             <HighlightSpanComp highlight={highlight}>full-stack developer</HighlightSpanComp>
             {` based in Wisconsin holding an Associate's in `}
-            <HighlightSpanComp highlight={highlight}>Web and Software Development</HighlightSpanComp>
+            <HighlightSpanComp highlight={highlight}>Web and Software Development.</HighlightSpanComp>
             {` I'm most experienced with building `}
             <HighlightSpanComp highlight={highlight}>responsive, user-friendly</HighlightSpanComp>
             {` web applications using modern technologies across both front-end and back-end stacks. I practice writing clean code, implementing intuitive design, and solving real-world problems through development.`}
