63a74f7f55910fa32718b48f065bcd0ce78a9fb7
Eli Sipos <elijsipos@gmail.com>
2025-05-16 18:15:40 -0500

    minor tweaks to the ui

--- src/components/ProfileComp.jsx
+++ src/components/ProfileComp.jsx
@@ -8,38 +8,38 @@ const ProfileComp = () => {
 
   return (
     <div className="flex justify-center mt-5">
-
-      <div className={`${theme == 'light' ? 'bg-black/5': 'bg-white/10'} flex p-5 rounded-2xl`}>
-      
+      <div className={`${theme == 'light' ? 'bg-black/5': 'bg-white/10'} flex p-4 rounded-2xl max-w-xl w-full`}>
         <div className="flex items-center">
-          <div className={`${theme == 'light' ? 'border-black' : 'border-white/80'} lg:w-25 lg:h-25 w-30 h-30 rounded-full overflow-hidden border-2`}>
+          <div className={`${theme == 'light' ? 'border-black' : 'border-white/80'} w-30 h-30 rounded-full overflow-hidden border-2`}>
             <img src={profileImg} alt="Picture of Eli Sipos"/>
           </div>
         </div>
         
-        <div className={`${theme == 'light' ? 'text-black' : 'text-white/80'} flex items-center mt-5`}>
-          <div>
-            <div className="text-3xl mx-1 lg:mx-5 flex">Hi, I'm Eli Sipos.</div>
+        <div className={`${theme == 'light' ? 'text-black' : 'text-white/80'} flex items-center lg:items-start mt-5`}>
+          <div className="ml-5">
+            <div className="text-3xl w-full">Hi, I'm Eli Sipos.</div>
+            <div className="text-lg w-full">Full Stack Developer</div>
+            <div className="text-lg w-full">Wisconsin, USA 🇺🇸</div>
+            <div className="ml-5">
+              <div className="text-lg w-full">Available for work</div>
 
-            <div className="flex items-center mx-5 text-lg">
-              <span className="absolute animate-ping mx-1 mr-1.5 mt-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} className="bi bi-circle-fill" viewBox="0 0 16 16">
+              {/* <div className="flex items-center"> */}
+              <span className="absolute animate-ping -mx-4 -mt-4.5">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} viewBox="0 0 16 16">
                   <circle cx="8" cy="8" r="8"/>
                 </svg>
               </span>
-              <span className="relative mx-1 mr-1.5 mt-1">
-                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} className="bi bi-circle-fill" viewBox="0 0 16 16">
+              <span className="absolute -mx-4 -mt-4.5">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill={`${theme == 'light' ? 'green' : 'lime'}`} viewBox="0 0 16 16">
                   <circle cx="8" cy="8" r="8"/>
                 </svg>
               </span>
-              <div>Available for work</div>
+              {/* </div> */}
             </div>
 
           </div>
         </div>
-
       </div>
-
     </div>
   )
 }
