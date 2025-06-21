2677e0c8db6e2de340d7cdc402a1d4438de04b21
Eli Sipos <elijsipos@gmail.com>
2025-06-21 12:17:13 -0500

    removed the scale effect on the tech stack for improved UX. list elements would expand when hovered, hinting that they could be clicked and opened, but they cant.

--- src/components/Search/SearchListElem.jsx
+++ src/components/Search/SearchListElem.jsx
@@ -8,16 +8,13 @@ const SearchListElem = (prop) => {
 
   return (
     <>
-      <motion.div
-        key="li"
-        whileHover={ { scale: 1.05 } }
-        transition={ { duration: 0.1, ease: "easeInOut"} }
+      <div
         className={`${
           theme == 'light' ? 'text-black' : 'text-gray-300'
         } grid grid-cols-2 md:grid-cols-4 justify-items-center m-2 py-2 rounded-lg md:hover:cursor-default select-none`}>
         <li className="w-min md:col-span-2 col-span-1 content-center">{prop.svg}</li>
         <li className="max-w-xs md:col-span-2 col-span-1 content-center">{prop.id}</li>
-      </motion.div>
+      </div>
     </>
   )
 }
